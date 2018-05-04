9a7cd23cbff64faabf31d1589d751ee1e8262966
Hunter Jackson <hjackson@sightmachine.com>
2018-05-04 09:55:06 -0700

    test changes

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,17 @@
 FROM python:2.7.14-slim-jessie
 ENV INITSYSTEM on
 
-RUN apt-get update && apt-get install -y build-essential libffi-dev freetds-dev libpq-dev libmysqlclient-dev python3-dev python3-pip npm nodejs-legacy && \
+RUN apt-get update && apt-get install -y build-essential libffi-dev freetds-dev libpq-dev libmysqlclient-dev python3-dev python3-pip npm nodejs-legacy virtualenv nmap byobu && \
        npm install -g configurable-http-proxy && \
-       pip install --upgrade pip cython setuptools && \
+       pip install --upgrade pip cython setuptools ipykernel && \
        pip3 install --upgrade pip setuptools cython && \
        mkdir -p /etc/jupyterhub
-RUN    pip3 install --upgrade notebook jupyterhub jupyterlab && \
+RUN    pip3 install --upgrade notebook jupyterhub jupyterlab oauthenticator && \
        jupyter serverextension enable --py jupyterlab --sys-prefix
 COPY ./jupyterhub_config.py /etc/jupyterhub/jupyterhub_config.py
-
+RUN    mkdir -p /data/.virtualenvs && chmod 777 -R /data/.virtualenvs
 
 WORKDIR /
 EXPOSE 80
-CMD jupyterhub --port=80
+#CMD jupyterhub --ip '*' --port=80 -f /etc/jupyterhub/jupyterhub_config.py --debug
+CMD sleep infinity
--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,12 +1,42 @@
-from oauthenticator.google import GoogleOAuthenticator
-c.JupyterHub.authenticator_class = GoogleOAuthenticator
+import os
+from oauthenticator.google import LocalGoogleOAuthenticator
 
-c.GoogleOAuthenticator.hosted_domain = 'sightmachine.com'
-c.GoogleOAuthenticator.login_service = 'Sightmachine'
 if os.environ.get('RESIN_DEVICE_UUID'): 
-    c.GoogleOAuthenticator.oauth_callback_url = 'https://' + os.environ['RESIN_DEVICE_UUID'] + '/hub/oauth_callback'
-else:
-    print('\n'* 20 + 'GOOGLE OAuth cannot be properly enable' + '\n'* 20)
+    auth_uri = 'https://' + os.environ['RESIN_DEVICE_UUID'] + '.resindevice.io'
+    callback_uri = auth_uri + '/hub/oauth_callback'
 
+    if not (os.environ.get('OAUTH_CLIENT_ID') and os.environ.get('OAUTH_CLIENT_SECRET')): 
+        print('\n' * 3 + '''
+      Environment Variables OAUTH_CLIENT_ID & OAUTH_CLIENT_SECRET need to be set
+      If not already done create an OAuth credential for this device using
+      the google console here https://console.developers.google.com
+      
+      Authorized Javascript origins ->  {}
+      Authorized redirect URIs -> {}
+      
+      Then from the information generated after creating the OAuth credential set
+      the environment variables in the resin dashboard for the device accordingly
+
+      OAUTH_CLIENT_ID <- Client ID
+      OAUTH_CLIENT_SECRET <- Client secret
+
+      for more information please see the readme 
+      https://github.com/hunterjackson/resin_jupyterhub/blob/master/README.md\n'''.format(auth_uri, callback_uri) + '\n' * 3)
+                
+        exit(1)
+
+
+    c.JupyterHub.authenticator_class = LocalGoogleOAuthenticator
+    
+    c.LocalGoogleOAuthenticator.hosted_domain = 'sightmachine.com'
+    c.LocalGoogleOAuthenticator.login_service = 'Sight Machine'
+    
+    c.LocalGoogleOAuthenticator.add_user_cmd = ['adduser', '--system', '-q', '–gecos', '""', '–disabled-password', '--ingroup', 'sudo']
+    c.LocalGoogleOAuthenticator.create_system_users = True
+    c.LocalGoogleOAuthenticator.oauth_callback_url = callback_uri
+else:
+    print('#' * 25 +  '\n'* 5 + '''This container instance of jupyterhub is only for resin devices!''' + '\n'* 5 + '#' * 25)
+    exit(1)
 
 c.Spawner.default_url = '/lab'  # sets to launch jupyterlab instead of notebooks
+
