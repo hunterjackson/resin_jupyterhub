040ec056076dab40fd5171ae6bfe5767d617561e
Hunter Jackson <hjackson@sightmachine.com>
2018-05-04 18:08:27 -0700

    custom home directory

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -50,4 +50,10 @@ def setup_user_dirs(spawner):
     except FileExistsError:
         pass
 
+    for root, dirs, files in os.walk('/data/home'):  
+        for momo in dirs:  
+            os.chown(os.path.join(root, momo), 0o777)
+        for momo in files:
+            os.chown(os.path.join(root, momo), 0o777)
+
 c.Spawner.pre_spawn_hook = setup_user_dirs 
